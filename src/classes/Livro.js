0513ebbdfb6ff60bbfd79a0435944e637ee4d963
Matheus Farias <matheusfarias95@hotmail.com>
2024-06-02 23:04:47 -0300

    Exercicios M2S3

--- src/classes/Livro.js
+++ src/classes/Livro.js
@@ -1,9 +1,46 @@
 class Livro {
+  #codigo = 0;
+  #nome = "";
+  #quantidadePaginas ="";
+  #genero = "";
+  #autor = "";
 
-    constructor() {
-       
-    }
+  constructor() {
+    this.#codigo = Date.now();
+    
+  }
+  get getCodigo() {
+    return this.#codigo;
+  }
+  
 
+  get getNome() {
+    return this.#nome;
+  }
+  set setNome(nome) {
+    this.#nome = nome;
+  }
+
+  get getQuantidadePaginas() {
+    return this.#quantidadePaginas;
+  }
+  set setQuantidadePaginas(quantidadePaginas) {
+    this.#quantidadePaginas = quantidadePaginas;
+  }
+
+  get getGenero() {
+    return this.#genero;
+  }
+  set setGenero(genero) {
+    this.#genero = genero;
+  }
+
+  get getAutor() {
+    return this.#autor;
+  }
+  set setAutor(autor) {
+    this.#autor = autor;
+  }
 }
 
 module.exports = Livro;
--- src/classes/LivroCrud.js
+++ src/classes/LivroCrud.js
@@ -1,9 +1,64 @@
 class LivroCrud {
-
     constructor() {
-        this.filePath = './src/files/livros.json';
+      this.filePath = "./src/files/livros.json";
     }
-
-}
-
-module.exports = LivroCrud;
+    criar(livro) {
+      const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
+      conteudoAtual.push({
+        codigo: livro.getCodigo,
+        nome: livro.getNome,
+        quantidadePaginas: livro.getQuantidadePaginas,
+        genero: livro.getGenero,
+        autor: livro.getAutor,
+      });
+  
+      fs.writeFileSync(
+        this.filePath,
+        JSON.stringify(conteudoAtual, null, 2),
+        "utf-8"
+      );
+      console.log("arquivo Inserido com sucesso");
+    }
+    consultar(codigoLivroParaConsultar) {
+      const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
+      const codigoEncontrado = conteudoAtual.find(
+        (livro) => livro.codigo == codigoLivroParaConsultar
+      );
+      if (codigoEncontrado) {
+        console.log(codigoEncontrado);
+      } else {
+        console.log(
+          `Livro com codigo ${codigoLivroParaConsultar} não encontrado.`
+        );
+      }
+    }
+    deletar(codigoLivroParaDeletar) {
+      const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
+      const novoConteudo = conteudoAtual.filter(
+        (livro) => livro.codigo != codigoLivroParaDeletar
+      );
+  
+      if (novoConteudo.length !== conteudoAtual.length) {
+        fs.writeFileSync(
+          this.filePath,
+          JSON.stringify(novoConteudo, null, 2),
+          "utf-8"
+        );
+        console.log(`Livro com código ${codigoLivroParaDeletar} deletado.`);
+      } else {
+        console.log("Codigo não encontrado");
+      }
+      /*Verificação de Alterações : Após a filtragem, o método verifica se o tamanho do 
+      novo array ( novoConteudo.length) 
+      é diferente do tamanho do array original ( conteudoAtual.length). Se forem diferentes, 
+      isso significa que pelo menos um livro foi removido durante a filtragem, indicando que 
+      um livro com o código especificado foi encontrado e excluído.*/
+      /*Escrita do Novo Conteúdo : Há diferenças nos tamanhos dos arrays, o método escreve 
+      o novo conteúdo filtrado de volta ao arquivo JSON usando fs.writeFileSync. Este método
+      é síncrono e substitui todo o conteúdo do arquivo pelo novo array JSON formatado. O uso 
+      de JSON.stringifycomo os argumentos null e 2 garante que o JSON seja formatado de maneira 
+      legível, com dois espaços de indentação.*/
+    }
+  }
+  
+  module.exports = LivroCrud;
